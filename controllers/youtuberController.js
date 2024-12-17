177954a6361e29b2a402aad951d5784615baa478
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2024-12-17 13:34:39 +0530

    'encrypted-referral-code'

--- controllers/youtuberController.js
+++ controllers/youtuberController.js
@@ -1,4 +1,29 @@
+const crypto = require('crypto');
 const Youtuber = require('../models/Youtuber');
+
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || '12345678901234567890123456789012'; // 32 characters for AES-256
+const IV_LENGTH = 16; // AES block size
+
+// Function to encrypt a value
+function encrypt(text) {
+  let iv = crypto.randomBytes(IV_LENGTH);
+  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+// Function to decrypt a value
+function decrypt(text) {
+  let textParts = text.split(':');
+  let iv = Buffer.from(textParts.shift(), 'hex');
+  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
 
 exports.addYouTuber = async (req, res) => {
   const { name, email, referralCode, commissionRate } = req.body;
@@ -19,8 +44,10 @@ exports.addYouTuber = async (req, res) => {
       });
     }
 
+    const encryptedReferralCode = encrypt(referralCode);
+
     // Check if referral code already exists
-    const existingByReferralCode = await Youtuber.findOne({ referralCode });
+    const existingByReferralCode = await Youtuber.findOne({ encryptedReferralCode });
     if (existingByReferralCode) {
       return res.status(400).json({ message: 'This referral code already exists. Please use a different referral code.' });
     }
@@ -29,7 +56,7 @@ exports.addYouTuber = async (req, res) => {
     const newYouTuber = new Youtuber({
       name,
       email,
-      referralCode,
+      referralCode : encryptedReferralCode,
       commissionRate: commissionRate || 70, // Default to 10% if not provided
     });
 
@@ -38,7 +65,7 @@ exports.addYouTuber = async (req, res) => {
     res.status(201).json({
       message: 'YouTuber added successfully',
       youTuber: newYouTuber,
-      url: `https://elixzor-ebook.com/?referrer=${referralCode}`
+      url: `https://elixzor-ebook.com/?referrer=${encryptedReferralCode}`
     });
   } catch (error) {
     // Handle other errors
